dbc4042fbd74b9e0630364464fc50469b1967165
Taofeeq Adebayo Ibrahim <32274017+Taofeeq90@users.noreply.github.com>
2025-09-29 10:34:08 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -87,5 +87,100 @@ output "is_default" {
 
 
 
+resource "azurerm_machine_learning_inference_cluster" "this" {
+  name                          = var.settings.name
+  kubernetes_cluster_id         = var.settings.kubernetes_cluster_id
+  location                      = var.settings.location
+  machine_learning_workspace_id = var.settings.machine_learning_workspace_id
+
+  cluster_purpose = try(var.settings.cluster_purpose, null)
+  description     = try(var.settings.description, null)
+
+  dynamic "identity" {
+    for_each = var.settings.identity != null ? [var.settings.identity] : []
+    content {
+      type         = identity.value.type
+      identity_ids = try(identity.value.identity_ids, null)
+    }
+  }
+
+  dynamic "ssl" {
+    for_each = var.settings.ssl != null ? [var.settings.ssl] : []
+    content {
+      cert                     = try(ssl.value.cert, null)
+      cname                    = try(ssl.value.cname, null)
+      key                      = try(ssl.value.key, null)
+      leaf_domain_label        = try(ssl.value.leaf_domain_label, null)
+      overwrite_existing_domain = try(ssl.value.overwrite_existing_domain, null)
+    }
+  }
+
+  tags = try(var.settings.tags, null)
+}
+
+
+output "id" {
+  value       = azurerm_machine_learning_inference_cluster.this.id
+  description = "The ID of the Machine Learning Inference Cluster."
+}
+
+output "identity" {
+  value = azurerm_machine_learning_inference_cluster.this.identity
+  description = "The Managed Service Identity information for the Inference Cluster."
+}
+
+output "principal_id" {
+  value       = try(azurerm_machine_learning_inference_cluster.this.identity[0].principal_id, null)
+  description = "The Principal ID for the Service Principal associated with the Managed Identity."
+}
+
+output "tenant_id" {
+  value       = try(azurerm_machine_learning_inference_cluster.this.identity[0].tenant_id, null)
+  description = "The Tenant ID for the Service Principal associated with the Managed Identity."
+}
+
+
+resource "azurerm_machine_learning_synapse_spark" "this" {
+  name                          = var.settings.name
+  machine_learning_workspace_id = var.settings.machine_learning_workspace_id
+  location                      = var.settings.location
+  synapse_spark_pool_id         = var.settings.synapse_spark_pool_id
+
+  description        = try(var.settings.description, null)
+  local_auth_enabled = try(var.settings.local_auth_enabled, null)
+
+  dynamic "identity" {
+    for_each = var.settings.identity != null ? [var.settings.identity] : []
+    content {
+      type         = identity.value.type
+      identity_ids = try(identity.value.identity_ids, null)
+    }
+  }
+
+  tags = try(var.settings.tags, null)
+}
+
+output "id" {
+  value       = azurerm_machine_learning_synapse_spark.this.id
+  description = "The ID of the Machine Learning Synapse Spark."
+}
+
+output "identity" {
+  value       = azurerm_machine_learning_synapse_spark.this.identity
+  description = "The Managed Service Identity information for the Machine Learning Synapse Spark."
+}
+
+output "principal_id" {
+  value       = try(azurerm_machine_learning_synapse_spark.this.identity[0].principal_id, null)
+  description = "The Principal ID for the Managed Identity."
+}
+
+output "tenant_id" {
+  value       = try(azurerm_machine_learning_synapse_spark.this.identity[0].tenant_id, null)
+  description = "The Tenant ID for the Managed Identity."
+}
+
+
+
 
 
